c6b43926e5a103af03783f80ba38ee5b247a8df2
林宥丞 <a26913700@gmail.com>
2018-10-22 00:32:38 +0800

    第一個路由

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel 快速入門 - 基本</title>
+
+    <!-- CSS 及 JavaScript -->
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Navbar 內容 -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: Monster
+ * Date: 2018/10/22
+ * Time: 上午 12:13
+ */
